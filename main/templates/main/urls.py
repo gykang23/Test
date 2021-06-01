2cbc8942426e6dfcf0b86bb31fd5791194ef6cb6
LIMJAEHUN <ijhgun007@naver.com>
2021-06-01 15:18:13 +0900

    hi

---
+++ main/templates/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+from . import views
+
+app_name='main'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('',index),
+    path('blog/',blog),
+    path('blog/new_post/', new_post),
+]
